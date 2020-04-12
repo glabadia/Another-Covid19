5bc37468bfa4f35d07ad290331e082a05a7901cd
Carlo Labadia <48218467+glabadia@users.noreply.github.com>
2020-04-12 20:56:12 +0800

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,23 @@ import styles from "./App.module.css";
 
 import { Cards, Chart, CountryPicker } from "./components";
 
+import { fetchData } from "./api";
+
 class App extends React.Component {
+  state = {
+    data: {}
+  };
+
+  async componentDidMount() {
+    const rawData = await fetchData();
+    this.setState({ data: rawData });
+  }
+
   render() {
+    const { data } = this.state;
     return (
       <div className={styles.container}>
-        <Cards />
+        <Cards data={data} />
         <CountryPicker />
         <Chart />
       </div>
--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 
-const Cards = () => {
+const Cards = props => {
+  console.log(props);
+
   return <div>Cards</div>;
 };
 
